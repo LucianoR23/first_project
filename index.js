e129dc48fed39e6a7d8731aef736c8bd51908a55
Luciano Rodriguez <231097lr@gmail.com>
2022-11-10 10:00:55 -0500

    Agregue todo lo necesario, falta terminar css y seguir con html y js

---
+++ index.js
@@ -0,0 +1,21 @@
+const combo1 = [
+    {nombre: "Combo 1"},
+    {nombre: "Fernet Branca", precio: 35, categoria: "Licor amargo"},
+    {nombre: "Two Hearted Ale 12pk", precio: 22, categoria: "Cerveza IPA"},
+    {nombre: "Tanqueray Nº10", precio: 33, categoria: "Gin"},
+    {nombre: "Coca de vidrio", precio: 5, categoria: "Gaseosa"},
+    {nombre: "2 Agua tonica", precio: 8, categoria: "Gaseosa"},
+]
+
+const combo2 = [
+    {nombre: "Combo 2"},
+    {nombre: "Macallan 12 Yr Double Cask", precio: 70, categoria: "Scotch Whiskey"},
+    {nombre: "Patron Añejo", precio: 50, categoria: "Tequila"},
+]
+
+function compra() {
+    let decisionCompra = prompt("Que combo desea comprar?").toLowerCase;
+    if (decisionCompra === "combo 1") {
+        
+    }
+}
